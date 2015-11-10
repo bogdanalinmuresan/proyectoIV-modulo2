29e6cb52af8bc907c6f5ab70b7f5624304104684
Bogdan Alin <alinugr@gmail.com>
2015-11-10 16:05:03 +0100

    actualizado

---
+++ app/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from django.conf.urls import patterns, include
+from . import views
+from app import views
+
+
+#from .views import RegistrarUsuario, RedirigirUsuario
+
+
+
+urlpatterns = [
+               #url(r'^$', views.libros, name='libros'),
+
+    url(r'^$', views.index, name='index'),
+    url(r'^libros/$', views.libros, name='libros'),
+]
